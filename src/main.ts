5d3183406c920f309a92fdaf09b7305d874d27b4
Ysmael Bolivar <ysmaelalejandro2@gmail.com>
2026-01-13 01:41:30 -0400

    cambiios 13/01/

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,35 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { initializeApp } from "firebase/app";
+import {useAbly} from './services/ably'
+
+//inicializando frirebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCtRfNVafsh2GtemFcIpQ-KX9sYHLDW38A",
+  authDomain: "curso-vue-f9f41.firebaseapp.com",
+  projectId: "curso-vue-f9f41",
+  storageBucket: "curso-vue-f9f41.firebasestorage.app",
+  messagingSenderId: "148206399730",
+  appId: "1:148206399730:web:8cce8a710240e7195c606b",
+  measurementId: "G-V7QSC44K91"
+};
+
+
+initializeApp(firebaseConfig);
+
+
+//iniciealizando ably
 
 const pinia = createPinia()
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 const app = createApp(App)
-
-app.use(pinia).use(router).mount('#app')
+app.use(useAbly)
+app.use(pinia).use(vuetify).use(router).mount('#app')
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,12 @@
+import { env } from 'process'
+
 /* eslint-disable */
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+declare module 'vue-router'
+
+declare module 'ably/app' {
+  export function initializeAbly(): void}
