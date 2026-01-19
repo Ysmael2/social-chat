802055cb91920a697fce5e2da9e83ca1b0c56767
Ysmael <ysmaelalejandro2@gmail.com>
2026-01-19 17:00:03 -0400

    cambiso

--- src/components/FriendsView.vue
+++ src/components/FriendsView.vue
@@ -1,8 +1,36 @@
 <template>
-    <br>
     <br><br><br><br>
-<div>
-    <h1>Lista de amigos</h1>
+<div class="friends-container">
+
+    <div class="header pa-4">
+        <h1 class="text-h5">Solicitudes de amistad</h1> 
+        <div class="d-flex align-center">
+            <v-container class="pa-0">
+                
+                
+                <v-chip variant="outlined" color="primary">
+                    pendientes
+                </v-chip>
+            </v-container>
+        </div>
+    </div>
+
+    <!-- contenido principal -->
+
+    <v-container fluid>
+        <v-row>
+            <v-col cols="12" md="8">
+                <v-card class="mb-4" rounded="lg" >
+
+                </v-card>
+
+
+            </v-col>
+        </v-row>
+
+
+    </v-container>
+
 </div>
 </template>
 
@@ -11,4 +39,8 @@
 </script>
 
 <style scoped>
+.friends-container {
+    min-height: calc(100vh - 64px);
+    background-color: transparent;
+}
 </style>
